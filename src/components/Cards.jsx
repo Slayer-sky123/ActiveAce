dc98ebb6a50fd65f2628d30efcc16c590afeaf69
Sandeep <sv181285@gmail.com>
2023-04-19 06:10:43 +0530

    modified plans

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,17 +4,21 @@ export default function Cards() {
   return (
  <div className="grid grid-cols-4 gap-10 m-10" >
   <div class="p1">
-<img src="./imgs/campfit.png"></img>
+<img src="./imgs/max1.jpeg"></img>
+<h1>CrossFit</h1>
   </div>
   <div>
-<img src="./imgs/maxfit.png"></img>
+<img src="./imgs/max2.jpeg"></img>
+<h1>Weight loss</h1>
   </div>
   <div>
-<img src="./imgs/powerfit.png"></img>
+<img src="./imgs/max3.jpeg"></img>
+<h1>Muscle Building</h1>
   </div>
 
   <div>
-<img src="./imgs/spinfit.png"></img>
+<img src="./imgs/max4.jpeg"></img>
+<h1>Zumba classes</h1>
   </div>
 
    </div>
